ea9aa8e512f8f3e54d678ada9d04758794a269cd
yahyahujatullah1-glitch <yahyahujatullah1@gmail.com>
2025-12-07 21:33:01 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,5 @@
-// js/config.js
-
-// Your Project URL
+// Paste your keys here
 const SUPABASE_URL = 'https://riluxnxxndwocrjuwzpd.supabase.co';
-
-// Your Public Anon Key (Correctly pasted)
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbHV4bnh4bmR3b2NyanV3enBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwOTM1MDEsImV4cCI6MjA4MDY2OTUwMX0.xcLBQijXfbCB3dM1FH4uzo08IPs-trovOy6T_vdpc_o';
-
-// Safety Check
-if (!window.supabase) {
-    console.error('Supabase library not loaded. Check index.html');
-}
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
